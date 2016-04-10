26855e5f7fbfab6098961dffa53367900d7a7829
Charlie Lee <clcblee@gmail.com>
2016-04-09 20:02:28 -0700

    Create distance calculation helper method

--- app/helpers/distances.rb
+++ app/helpers/distances.rb
@@ -1,5 +1,22 @@
 # write a method that takes two coordinate pairs as arguments,
 # and returns the distance between the two points
 def calculate_distance(a, b)
-
+  Math.sqrt((a.first-b.first)**2 + (a.last-b.last)**2)
 end
+
+def closest_distance(start, array)
+  closest_place_index = 0
+  shortest_distance = calculate_distance(start, array.first)
+  array.each_with_index do |location, index|
+    this_distance = calculate_distance(location, start)
+    if this_distance < shortest_distance
+      closest_place_index = index
+    end
+  end
+  closest_place_index
+end
+
+a = [0,0]
+array = [ [5,5], [10,10], [3,3], [1,1] ]
+
+p closest_distance(a, array)
--- mvp_overview.md
+++
@@ -1,5 +0,0 @@
-## MVP
-1. User opens application
-2. User can choose what service they need from list of options
-3. User inputs their current location
-4. App displays route with directions to nearest location offering that service
